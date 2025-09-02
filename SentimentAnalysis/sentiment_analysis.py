14c92af3a9690433f6af1e9e2b1a02ca8e9592f9
Attila Orosz <attila.orosz.dev@gmail.com>
2025-09-02 19:04:56 +0400

    Task 7, part 1

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,12 +1,25 @@
 import requests
-import json
 
-def sentiment_analyzer(text_to_analyse):
-    url = "https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    my_obj = {"raw_document": {"text": text_to_analyse}}
-    response = requests.post(url, json=my_obj, headers=header)
-    formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
-    return {'label': label, 'score':score}
+# Define the URL for the sentiment analysis API
+url = "https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
+
+# Set the headers with the required model ID for the API
+headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+
+# Define the first payload with nonsensical text to test the API
+myobj = {"raw_document": {"text": "as987da-6s2d aweadsa"}}
+
+# Make a POST request to the API with the first payload and headers
+response = requests.post(url, json=myobj, headers=headers)
+
+# Print the status code of the first response
+print(response.status_code)
+
+# Define the second payload with a meaningful text to test the API
+myobj = {"raw_document": {"text": "Testing this application for error handling"}}
+
+# Make a POST request to the API with the second payload and headers
+response = requests.post(url, json=myobj, headers=headers)
+
+# Print the status code of the second response
+print(response.status_code)
