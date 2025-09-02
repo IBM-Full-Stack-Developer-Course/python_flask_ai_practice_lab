85ef9a9ec6b1c51c1971cf89bbafb51d05c88787
Attila Orosz <attila.orosz.dev@gmail.com>
2025-09-02 18:28:17 +0400

    Added some simple unit tests

---
+++ test_sentiment_analysis.py
@@ -0,0 +1,19 @@
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+import unittest
+
+class TestSentimentAnalyzer(unittest.TestCase):
+    def test_sentiment_analyzer(self):
+        case_1 = "I love working with Python"
+        case_2 = "I love working with Python"
+        case_3 =  "I am neutral on Python"
+
+        result = sentiment_analyzer(case_1)
+        self.assertEqual(result['label'], 'SENT_POSITIVE')
+
+        result_2 = sentiment_analyzer(case_2)
+        self.assertEqual(result['label'], 'SENT_NEGATIVE')
+
+        result_3 = sentiment_analyzer(case_3)
+        self.assertEqual(result['label'], 'SENT_NEUTRAL')
+
+unittest.main()
